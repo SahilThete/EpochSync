b7454afbbe070a2ec1f81d902244344e5d423488
Sahil Thete <sahilthete.st@gmail.com>
2026-07-30 00:04:20 +0530

    Update version to 0.2.0 and reorder module definitions in Modules.h

--- src/common/Modules.h
+++ src/common/Modules.h
@@ -5,20 +5,22 @@
 
 #define MODULE_SYSTEM           "System"
 
-#define MODULE_CONFIG           "Config"
-
 #define MODULE_NETWORK          "Network"
 
 #define MODULE_NTP              "NTP"
 
 #define MODULE_RTC              "RTC"
 
+#define MODULE_TIMEMANAGER      "TimeManager"
+
+#define MODULE_CONFIG           "Config"
+
 #define MODULE_LAUNCHER         "Launcher"
 
 #define MODULE_UI               "UI"
 
-#define MODULE_TIMEMANAGER      "TimeManager"
+#define MODULE_GRAPHICS         "Graphics"
 
-// #define MODULE_LOGGER           "Logger"
+#define MODULE_LOGGER           "Logger"
 
 #endif /* EPOCHSYNC_MODULES_H */
